fb1c8544a6f21cb3dd5ef063ef5c7e31ebbd699b
skultx <151526964+skultx@users.noreply.github.com>
2024-01-25 11:38:19 +0100

    cambio1

---
+++ HolaDam/src/practica2/HolaDam.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica2;
+
+/**
+ *
+ * @author delcrego
+ */
+public class HolaDam {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+    
+}
