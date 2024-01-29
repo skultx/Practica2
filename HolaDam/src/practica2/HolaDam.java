3da9aa49cb2d2415ec162a22fa09adafb611bcdd
lopmenal <alexetelopez@gmail.com>
2024-01-29 09:22:44 +0100

    Cambio3

--- HolaDam/src/practica2/HolaDam.java
+++ HolaDam/src/practica2/HolaDam.java
@@ -11,6 +11,7 @@ package practica2;
 public class HolaDam {
     public static void main(String[] args) {
         System.out.println("hola");
+        System.out.println("Mergeada");
     }
     
 }
